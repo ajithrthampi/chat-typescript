53d947a64db8bb983af28c354403b230b1166648
phozpho <ajithrthampi999@gmail.com>
2023-06-07 16:15:13 +0530

    Loading

--- my-app/src/components/message/Message.tsx
+++ my-app/src/components/message/Message.tsx
@@ -1,8 +1,8 @@
     import React, { useEffect, useRef, useState } from 'react'
     import Messages from './Messages'
     import axios from 'axios'
-    import { debounce, throttle } from "lodash"
-
+import Loading from '../loading/Loading'
+  
     const Message = () => {
 
       const [data, setData] = useState<Array<[]>>([])
@@ -59,11 +59,11 @@
           
           divRef.current.addEventListener('scroll', handleScroll);
         }
-        // return () => {
-        //   if (divRef.current) {
-        //     divRef.current.removeEventListener('scroll', handleScroll);
-        //   }
-        // };
+        return () => {
+          if (divRef.current) {
+            divRef.current.removeEventListener('scroll', handleScroll);
+          }
+        };
       }, []);
     
       
@@ -71,10 +71,10 @@
       return (
         <>
           <div>
-            <div className=' overflow-scroll z-0 absolute right-0 left-0 top-[135px] bottom-20'
+            <div className=' overflow-scroll z-0 absolute right-0 left-0 top-[135px] bottom-20 '
               ref={divRef}
             >
-              {loading && <div className='text-2xl'>Loading</div>}
+              {loading && <div className='text-2xl flex justify-center items-center'><Loading /></div>}
               {
                 data.length > 0 && data.map((value: any, index: number) => (
                   <div key={index}
